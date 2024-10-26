0f4423464c1f4b58bd033bc65abfc3640bce4831
Dan Panaete <dani.panaete@gmail.com>
2024-10-26 15:03:33 +0300

    feature: admin page table with add/delete item

--- api/alimente.js
+++ api/alimente.js
@@ -1,7 +1,47 @@
+
 const URL = "https://670fe5b1a85f4164ef2c621e.mockapi.io/alimente"; // endpoint -> asa comunicam cu baza de date
 
 export async function getAllProducts() {
     const response = await fetch(URL);
     const products = await response.json();
+  
     return products;
-}
+  }
+  
+  export async function getProductById(id) {
+    const response = await fetch(`${URL}/${id}`);
+    const product = await response.json();
+  
+    return product;
+  }
+  
+  export async function addNewProduct(product) {
+    const response = await fetch(URL, {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  
+    const newProduct = await response.json();
+    return newProduct;
+  }
+  
+  export async function updateProduct(product, id) {
+    const response = await fetch(`${URL}/${id}`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(product),
+    });
+  
+    const editedProduct = await response.json();
+  
+    return editedProduct;
+  }
+  
+  export async function deleteProduct(id) {
+    await fetch(`${URL}/${id}`, { method: "DELETE" });
+  }
